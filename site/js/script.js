5f786ccdde5152a0a041fb3d2e6f27031ebaffc4
Marti Mosetti <marti.mosetti@gmail.com>
2021-01-31 17:55:04 +0100

    Completed assignment4

---
+++ site/js/script.js
@@ -0,0 +1,15 @@
+// x = "Hello ballers!"
+
+var x;
+console.log(x);
+
+if (x == undefined) {
+    console.log("x is undefined")
+}
+
+if (x == undefined) {
+    console.log("x is undefined")
+}
+else {
+    console.log("x is defined and equals to " + x)
+}
